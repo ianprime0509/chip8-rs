3106def14cdef6d4f73532d25f91e4d2e69b9543
Ian Johnson <ianprime0509@gmail.com>
2018-02-19 11:48:28 -0500

    Relicense to MIT

--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,10 @@
-// Copyright 2018 Ian Johnson
-
-// This file is part of Chip-8.
-
-// Chip-8 is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Chip-8 is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Chip-8.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * Copyright 2018 Ian Johnson
+ *
+ * This is free software, distributed under the MIT license.  A copy of the
+ * license can be found in the LICENSE file in the project root, or at
+ * https://opensource.org/licenses/MIT.
+ */
 
 //! Various utility functions.
 
